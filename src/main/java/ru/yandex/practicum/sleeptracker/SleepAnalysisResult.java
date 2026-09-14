6dfa90960b24e83af13a43a55e0013397defc796
Artem Lagoshny <Artem.investor2021@yandex.tu>
2026-09-14 15:38:54 +0300

    Устранить ошибки checkstyle в SleepAnalysisResult

--- src/main/java/ru/yandex/practicum/sleeptracker/SleepAnalysisResult.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/SleepAnalysisResult.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.sleeptracker;
 
 public class SleepAnalysisResult {
-   private final String description;
-   private final Object value;
+    private final String description;
+    private final Object value;
 
-    public SleepAnalysisResult(String description, Object value){
+    public SleepAnalysisResult(String description, Object value) {
         this.description = description;
         this.value = value;
     }
@@ -18,14 +18,11 @@ public class SleepAnalysisResult {
 
         return value;
     }
+
     @Override
-    public String toString(){
+    public String toString() {
 
         return description + ": " + value;
     }
-
-
-
-
 
 }
